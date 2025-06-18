cfc799d236ef84a7f0fc0cc3d683327851984a9e
rhtnddd <qlalfql123@gmail.com>
2025-06-18 12:10:09 +0900

    함수 과제

---
+++ 과제5/함수의 매개변수.py
@@ -0,0 +1,25 @@
+# def pyfun():
+#     print('Python is Fun')
+# pyfun()
+# def squ(n):
+#     print(f'{n*n}')
+# squ(3)
+# def print_times(value,n):
+#     for a in range(n):
+#         print(value)
+
+# print_times("안녕",5)
+# def get_max(a,b):
+#     n=max(a,b)
+#     print(n)
+# get_max(10,20)
+
+# def square(n):
+#     return n*n
+# x=square(4)
+# print(x)
+
+def get_max(a,b):
+    return max(a,b)
+x=get_max(7,5)
+print(x)

f64a13de9ca7c3ccff5c65ee3e844916c1f2deb5
rhtnddd <qlalfql123@gmail.com>
2025-07-02 23:53:55 +0900

    연습두번째

--- 내가 쓰는곳/시험연습.py
+++ 내가 쓰는곳/시험연습.py
@@ -1,9 +1,21 @@
-def sel_sort(a):
-    for i in range(0,len(a)-1):
-        for j in range(i+1,len(a)):
-            if a[i]>a[j]:
-                a[i],a[j]=a[j],a[i]
+# def sel_sort(a):
+#     for i in range(0,len(a)-1):
+#         for j in range(i+1,len(a)):
+#             if a[i]>a[j]:
+#                 a[i],a[j]=a[j],a[i]
+#     return a
+# d = [2, 4, 5, 1, 3]
+# sel_sort(d)
+# print(d)
+def ins_sort(a):
+    for i in range(0,len(a)):
+        key=a[i]
+        j=i-1
+        while j>=0 and a[j]>key:
+            a[j+1]=a[j]
+            j-=1
+        a[j+1]=key
     return a
 d = [2, 4, 5, 1, 3]
-sel_sort(d)
+ins_sort(d)
 print(d)
